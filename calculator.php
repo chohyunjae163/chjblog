a3b2070e2970f316b3ed0cb120cf4b817b3c2302
Hyun Jae Cho <50690938+chohyunjae163@users.noreply.github.com>
2022-03-07 15:06:17 +0000

    working on php calculator

--- calculator.php
+++ calculator.php
@@ -2,13 +2,27 @@
     declare(strict_types=1);
     function calculate($expression){
         $stack = array();
+        $result = 0;
         $str_len = strlen($expression);
         for($index = 0; $index < $str_len; $index++){
             $token = $expression[$index];
-            if($token == "/" || $token == "*")
-                array_pop($stack);
+            if($token == "/"){
+               
+            }else if($token == "*") {
+            }
+               
             else
                 array_push($stack,$token);
+        }
+        while(count($stack) > 0){
+            $element = array_pop($stack);
+            if ( $element == "+" ) {
+                $operand = array_pop($stack);
+                $result += $operand;
+            } else if ( $element == "-" ) {
+                $operand = array_pop($stack);
+                $result -= $operand;
+            }
         }
         return $stack;
     }
