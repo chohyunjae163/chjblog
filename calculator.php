398db3c13a982abb1179d26a5e5a37c2105b63a2
Hyun Jae Cho <50690938+chohyunjae163@users.noreply.github.com>
2022-03-09 02:35:23 +0000

    added multiplication.

--- calculator.php
+++ calculator.php
@@ -1,26 +1,36 @@
 <?php 
     declare(strict_types=1);
     function calculate(string $str){
-        $stack = explode(" ",$str);
+        $arr = explode(" ",$str);
         $result = 0;
-        while(count($stack) > 0){
-            $element = array_pop($stack);
-            if ( $element == "*" ) {
-
-            } else if ( $element == "/" ) {
-
-            } else if ( $element == "+" ) {
+        $stack = array();
+        for ( $index = 0; $index < count($arr); $index++ ) {
+            $element = $arr[$index];
+            if(is_numeric($element) || $element == "+" || $element == "-") {
+                array_push($stack,$arr[$index]);
+            }else if ( $element == "*" ) {
                 $operand = array_pop($stack);
+                $index += 1;
+                $operand_2 = $arr[$index];
+                $result = (float)$operand * $operand_2;
+            } else if ( $element == "/" ) {
+            } 
+        }
+        /*
+        while ( count($arr) > 0 ) {
+            $element = array_pop($arr);
+            if ( $element == "+" ) {
+                $operand = array_pop($arr);
                 $result = (float)$operand + $result;
             } else if ( $element == "-" ) {
-                $operand = array_pop($stack);
+                $operand = array_pop($arr);
                 $result = (float)$operand - $result;
-            } else if ( $element == ")" ) {
-                //while()
             } else if ( is_numeric($element) ) {
                 $result = $element;
             }
         }
+        */
+
         return "$str  = $result";
     }
 ?>
